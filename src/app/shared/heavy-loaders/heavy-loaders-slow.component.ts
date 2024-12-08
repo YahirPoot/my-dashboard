8e2ce498fb6b0372a5ac2059607e5edc2e3855f4
Angel Yahir Poot Ucan <135137591+YahirPoot@users.noreply.github.com>
2024-12-07 22:07:12 -0600

    creación de archivos y directorios

---
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heavy-loaders-slow',
+  imports: [],
+  template: `<h1>Heavy Loaders Slow</h1>`
+})
+export class HeavyLoadersSlowComponent {
+
+}
---
+++ src/app/shared/heavy-loaders/user-loader.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-loader',
+  imports: [],
+  template: `<h1>User Loader</h1>`
+})
+
+export class UserLoaderComponent {
+  constructor() { }
+
+}

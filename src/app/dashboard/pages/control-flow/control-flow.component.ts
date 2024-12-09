e2a863fdf859ed93b52a0c224b0b9d0d275023f8
Angel Yahir Poot Ucan <135137591+YahirPoot@users.noreply.github.com>
2024-12-09 16:10:40 -0600

    control flow @SWITCH

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,6 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { defaultUrlMatcher } from '@angular/router';
 
+type Grade = 'A' | 'B' | 'F';
+
 @Component({
   imports: [],
   templateUrl: './control-flow.component.html',
@@ -9,6 +11,7 @@ import { defaultUrlMatcher } from '@angular/router';
 export default class ControlFlowComponent {
 
   public showContent = signal(false);
+  public grade = signal<Grade>('A');
 
   public toggleContent() {
     this.showContent.update(value => !value);

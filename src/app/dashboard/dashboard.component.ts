da9ef7c7de31130224a6794630ea374944c83926
Angel Yahir Poot Ucan <135137591+YahirPoot@users.noreply.github.com>
2024-12-08 19:13:50 -0600

    Configuración del dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SideMenuComponent } from '../shared/side-menu/side-menu.component';
 
 @Component({
-  imports: [RouterModule],
+  standalone: true,
+  imports: [RouterModule, SideMenuComponent],
   templateUrl: './dashboard.component.html',
   styles: ``
 })
--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
+  standalone: true,
   selector: 'app-side-menu',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css'
 })
 export class SideMenuComponent {
 
+  public menuItems = routes
+    .map( route => route.children ?? [])
+    .flat()
+    .filter( route => route && route.path)
+    .filter( route => !route.path?.includes(':'));
 }

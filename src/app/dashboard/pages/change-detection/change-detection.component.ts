546f79c7b35f0e5ddcc683f4f140bcec03a7246f
Angel Yahir Poot Ucan <135137591+YahirPoot@users.noreply.github.com>
2024-12-12 18:59:43 -0600

    change detection - OnPush

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from "../../../shared/title/title.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [],
-  templateUrl: './change-detection.component.html',
-  styles: ``
+  standalone: true,
+  imports: [TitleComponent, CommonModule],
+  // el change detection on push solo se ejecutara cuando cambie el input
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <app-title [title]="currentFramework()" />
+
+    <pre> {{ frameworkAsSignal() | json }} </pre>
+    <pre> {{ frameworkAsProperty | json }} </pre>
+  `
 })
 export default class ChangeDetectionComponent {
 
+  public currentFramework = computed(
+    () => `Change Detection -  ${this.frameworkAsSignal().name}`
+  );
+
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releasDate: '2016',
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releasDate: '2016',
+  };
+
+  constructor() {
+
+    setTimeout(() => {
+
+      // this.frameworkAsProperty.name = 'React';
+
+      this.frameworkAsSignal.update(value => {
+        value.name = 'React';
+        return {...value};
+      })
+      console.log('Cambio hecho')
+    }, 3000);
+  }
 }

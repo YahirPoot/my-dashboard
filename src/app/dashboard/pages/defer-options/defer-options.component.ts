05aa76fd8d7344c4698aaa77354e5ee9294f9a93
Angel Yahir Poot Ucan <135137591+YahirPoot@users.noreply.github.com>
2025-01-14 00:09:37 -0600

    uso del defer trigger - on interaction

--- src/app/dashboard/pages/defer-options/defer-options.component.ts
+++ src/app/dashboard/pages/defer-options/defer-options.component.ts
@@ -1,7 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeavyLoadersFastComponent } from '@shared/heavy-loaders/heavy-loaders-fast.component';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
-  imports: [],
+  imports: [CommonModule, HeavyLoadersFastComponent, TitleComponent],
+  standalone: true,
   templateUrl: './defer-options.component.html',
   styles: ``
 })

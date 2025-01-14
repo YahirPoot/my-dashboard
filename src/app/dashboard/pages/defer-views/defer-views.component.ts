77ea8f1a3449bf92c6dcc2647d5db4c4df466d55
Angel Yahir Poot Ucan <135137591+YahirPoot@users.noreply.github.com>
2025-01-13 23:40:21 -0600

    uso del defer views

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { HeavyLoadersSlowComponent } from '@shared/heavy-loaders/heavy-loaders-slow.component';
+import { TitleComponent } from "../../../shared/title/title.component";
 
 @Component({
-  imports: [HeavyLoadersSlowComponent],
+  imports: [HeavyLoadersSlowComponent, TitleComponent],
   standalone: true,
   templateUrl: './defer-views.component.html',
 })
